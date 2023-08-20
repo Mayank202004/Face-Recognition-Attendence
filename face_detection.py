717831010969003794c554a09912a0a70e332c34
Mayank202004 <136550347+Mayank202004@users.noreply.github.com>
2023-08-20 12:25:14 +0530

    Add files via upload

---
+++ face_detection.py
@@ -0,0 +1,84 @@
+import pandas as pd
+import cv2
+import urllib.request
+import numpy as np
+import os
+import face_recognition
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="mayank",
+  database="attendance"
+)
+cursor=mydb.cursor()
+ 
+path = r'C:\Users\mayan\Downloads\attendance\image_folder'
+url='http://192.168.43.216/1600x1200.jpg'
+##'''cam.bmp / cam-lo.jpg /cam-hi.jpg / cam.mjpeg ''' 
+images = []
+classNames = []
+myList = os.listdir(path)
+print(myList)
+for cl in myList:
+    curImg = cv2.imread(f'{path}/{cl}')
+    images.append(curImg)
+    classNames.append(os.path.splitext(cl)[0])
+print(classNames)
+ 
+ 
+def findEncodings(images):
+    encodeList = []
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+ 
+ 
+def markAttendance(name):
+    x=(f"UPDATE att SET 01_03_2023='p' WHERE Name=(%s)")
+    cursor.execute(x,(name,))
+    mydb.commit()
+ 
+ 
+encodeListKnown = findEncodings(images)
+print('Encoding Complete')
+ 
+#cap = cv2.VideoCapture(0)
+ 
+while True:
+    #success, img = cap.read()
+    img_resp=urllib.request.urlopen(url)
+    imgnp=np.array(bytearray(img_resp.read()),dtype=np.uint8)
+    img=cv2.imdecode(imgnp,-1)
+# img = captureScreen()
+    imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+ 
+    facesCurFrame = face_recognition.face_locations(imgS)
+    encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
+ 
+    for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+# print(faceDis)
+        matchIndex = np.argmin(faceDis)
+ 
+        if matches[matchIndex]:
+            name = classNames[matchIndex].upper()
+# print(name)
+            y1, x2, y2, x1 = faceLoc
+            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
+            cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+            markAttendance(name)
+ 
+    cv2.imshow('Webcam', img)
+    key=cv2.waitKey(5)
+    if key==ord('q'):
+        break
+cv2.destroyAllWindows()
+cv2.imread
